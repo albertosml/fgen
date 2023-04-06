4628d12ccc126f478ec41dcd2e31bf2a811f6b98
Alberto Silvestre Montes Linares <albertosilvestremonteslinares@gmail.com>
2023-04-06 13:42:58 +0200

    TEMFLD-4: Do not allow ranges on spreadsheet positions

--- src/main/java/template/application/utils/SpreadsheetPositionValidator.java
+++ src/main/java/template/application/utils/SpreadsheetPositionValidator.java
@@ -21,12 +21,7 @@ public class SpreadsheetPositionValidator {
         // Example valid position: B13
         String validPositionRegex = "^[A-Z]+[1-9]+[0-9]*$";
 
-        // Example valid position range: B13:B19
-        String validRangeRegex = "^[A-Z]+[1-9]+[0-9]*[:]{1}[A-Z]+[1-9]+[0-9]*$";
-
-        String spreadsheetPositionRegex = String.format("%s|%s", validPositionRegex, validRangeRegex);
-
-        return RegexValidator.matches(position, spreadsheetPositionRegex);
+        return RegexValidator.matches(position, validPositionRegex);
     }
 
 }
